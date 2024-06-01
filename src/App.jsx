f33bf9c25e486739b571d91668e5e7289a87d93e
mazenwael4545 <mazenwaelpro.567@gmail.com>
2024-06-01 10:53:05 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import TodoForm from './Components/TodoForm'
 import Todo from './Components/Todo';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   let [todos, setTodos] = useState([]);
@@ -43,52 +43,53 @@ const App = () => {
   }
 
   return (
-    <Router basename='/todo-list'>
-      <Route path='/' element={
-            <div className="container">
-      <TodoForm onSubmit={addTodo} />
-      <div className='tabs'>
-        <button onClick={()=> setTodosToShow('all')}>all</button>
-        <button onClick={()=> setTodosToShow('active')}>active</button>
-        <button onClick={()=> setTodosToShow('complete')}>complete</button>
-      </div>
-      <div className="todo-container">
-        {todos.length === 0 ? (
-          <h2 className='no-tasks'>no tasks yet</h2>
-        ) : (
-          todos.map((todo) => (
-            <Todo
-              key={todo.id}
-              todo={todo}
-              onDelete={() => handleDelete(todo.id)}
-              toggleComplete={() => toggleComplete(todo.id)}
-            />
-          ))
-        )}
-      </div>
-      <div className="buttons">
-        {todos.some(todo=> todo.complete) ? 
-        (
-          <button onClick={()=> setTodos(todos.filter(todo=> !todo.complete))}>
-            remove all complete todos
-          </button>
-        )
-        : null}
-        <button onClick={()=> {
-          setToggleAllComplete(!toggleAllComplete);
-          setTodos(
-            todos.map( todo => ({
-              ...todo,
-              complete: toggleAllComplete
-            }))
-          )
-        }}>toggle all complete {`${toggleAllComplete}`}</button>
-      </div>
-    </div>
-      } />
-      <Route path='*' element={<h2>there is no route like that</h2>} />
+    <Router>
+      <Routes>
+        <Route path='/todo-list' element={
+          <div className="container">
+            <TodoForm onSubmit={addTodo} />
+            <div className='tabs'>
+              <button onClick={()=> setTodosToShow('all')}>all</button>
+              <button onClick={()=> setTodosToShow('active')}>active</button>
+              <button onClick={()=> setTodosToShow('complete')}>complete</button>
+            </div>
+            <div className="todo-container">
+              {todos.length === 0 ? (
+                <h2 className='no-tasks'>no tasks yet</h2>
+              ) : (
+                todos.map((todo) => (
+                  <Todo
+                    key={todo.id}
+                    todo={todo}
+                    onDelete={() => handleDelete(todo.id)}
+                    toggleComplete={() => toggleComplete(todo.id)}
+                  />
+                ))
+              )}
+            </div>
+            <div className="buttons">
+              {todos.some(todo=> todo.complete) ? 
+              (
+                <button onClick={()=> setTodos(todos.filter(todo=> !todo.complete))}>
+                  remove all complete todos
+                </button>
+              )
+              : null}
+              <button onClick={()=> {
+                setToggleAllComplete(!toggleAllComplete);
+                setTodos(
+                  todos.map( todo => ({
+                    ...todo,
+                    complete: toggleAllComplete
+                  }))
+                )
+              }}>toggle all complete {`${toggleAllComplete}`}</button>
+            </div>
+          </div>
+        } />
+        <Route path='/*' element={<h2>There is no route like that</h2>} />
+      </Routes>
     </Router>
-
   )
 }
 
